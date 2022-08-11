c114746688f1b16ca3c2a5e07613a71da1b50ad0
Dani3108 <danielberrozpe4@gmail.com>
2022-08-11 18:32:26 +0200

    Deleted redundant devDependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './styles.css'
+import captionHeader from './images/img-caption-header.png'
+import exampleGif from './images/example.gif'
 
 function App() {
   return (
     <div>
       <h1 className="title">r/whenthe generator</h1>
+      <img src={exampleGif}></img>
     </div>
   );
 }
